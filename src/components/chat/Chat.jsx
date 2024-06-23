697938ea8e1468e1095c1d03e0d2c1a567d05192
ashrith <ashrith.mudundi@gmail.com>
2024-06-23 18:36:47 +0530

    created chat components

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,97 @@
 import "./chat.css"
+import EmojiPicker from 'emoji-picker-react';
+import { useState } from "react";
 
 const Chat = () => {
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = e => {
+        setText(text + e.emoji)
+        setOpen(false)
+    }
+
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>John Doe</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+
+            
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Eum repudiandae consequatur ut aliquam placeat nobis tempora pariatur,
+                            non, unde at sit id blanditiis, asperiores libero illum obcaecati 
+                            delectus magni laudantium?
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="https://img.freepik.com/free-photo/village-background-asset-game-2d-futuristic-generative-ai_191095-2024.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Eum repudiandae consequatur ut aliquam placeat nobis tempora pariatur,
+                            non, unde at sit id blanditiis, asperiores libero illum obcaecati 
+                            delectus magni laudantium?
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Eum repudiandae consequatur ut aliquam placeat nobis tempora pariatur,
+                            non, unde at sit id blanditiis, asperiores libero illum obcaecati 
+                            delectus magni laudantium?
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" 
+                placeholder="Type a  message..." 
+                value={text}
+                onChange={e=>setText(e.target.value)}
+                />
+                <div className="emoji">
+                    <img src="./emoji.png"
+                    alt=""
+                    onClick={() => setOpen(prev=>!prev)}
+                    />
+                    <div className="picker">
+                        <EmojiPicker open = {open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
     )
 }
 
--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,8 +1,44 @@
-import "./chatList.css"
+import "./chatList.css";
+import { useState } from "react";
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
     return (
-        <div className="chatList">ChatList</div>
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt="" />
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <img
+                 src={addMode ? "./minus.png" : "./plus.png"}
+                 alt="" 
+                 className="add"
+                 onClick={() => setAddMode(!addMode)}
+                 />
+            </div>
+              <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+              </div>
+              <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+              </div>
+              <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>John Doe</span>
+                    <p>Hello</p>
+                </div>
+              </div>
+        </div>
     )
 }
 
